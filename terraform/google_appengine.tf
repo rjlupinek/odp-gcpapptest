21fc42719a381cd5241305fa7ced762fc384f083
RobertJLupinek <9854828608@gsa.gov>
2019-08-23 17:02:11 -0400

    Adding appengine terraform setup.

--- terraform/google_appengine.tf
+++ terraform/google_appengine.tf
@@ -1,11 +1,14 @@
-
+resource "google_app_engine_application" "app" {
+  project     = "${var.project_id}"
+  location_id = "${var.region}"
+}
 
 resource "google_app_engine_firewall_rule" "rule1000" {
   project = "${var.project_id}"
   priority = 1000
   description = "Terraform -  Allow GSA internal access."
   action = "ALLOW"
-  source_range = "35.235.0.0/16	"
+  source_range = "35.235.0.0/16"
 }
 
 resource "google_app_engine_firewall_rule" "rule1100" {
