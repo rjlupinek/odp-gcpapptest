3bd1b647b5952169be4de998e5a87661f7946b5a
RobertJLupinek <9854828608@GSA.GOV>
2019-08-29 15:56:13 -0400

    Adding monitoring via terraform.

--- terraform/google_monitoring.tf
+++ terraform/google_monitoring.tf
@@ -6,6 +6,7 @@ resource "google_monitoring_notification_channel" "email" {
   }
 }
 
+#Monitoring Connections
 resource "google_monitoring_alert_policy" "alert_policy_connections"{
   display_name = "pypostgresql-connection-limit-test"
   combiner= "OR"
@@ -19,7 +20,7 @@ resource "google_monitoring_alert_policy" "alert_policy_connections"{
         comparison = "COMPARISON_GT"
         duration = "60s"
         filter = "metric.type=\"appengine.googleapis.com/flex/connections/current\" resource.type=\"gae_app\" resource.label.\"project_id\"=\"i-ise-04302019-playground\" resource.label.\"module_id\"=\"pypostgresql\""
-        threshold_value = 2
+        threshold_value = 200
         trigger = {
           count = 1
         }
@@ -35,5 +36,56 @@ resource "google_monitoring_alert_policy" "alert_policy_connections"{
   ]
 }
 
-#Owner of the project
+
+resource "google_monitoring_alert_policy" "alert_policy_owner"{
+  display_name = "Project Ownership assignments-changes"
+  combiner= "OR"
+  conditions {
+      display_name = "logging/user/Project-Ownership-Assignments-Changes"
+      condition_threshold = {
+        aggregations {
+          alignment_period = "60s"
+          per_series_aligner = "ALIGN_RATE"
+        }
+        comparison = "COMPARISON_GT"
+        duration = "60s"
+        filter = "metric.type=\"logging.googleapis.com/user/Project-Ownership-Assignments-Changes\""
+        threshold_value = 1
+        trigger = {
+          count = 1
+        }
+      }
+  }
+  documentation = {
+    content = "# Warning\n\n## Error message:\n\nThe project ownership has changed.\nThis alert policy is configured via Terraform."
+    mime_type = "text/markdown"
+  }
+}
+
+
+resource "google_monitoring_alert_policy" "alert_policy_instance_count"{
+  display_name = "Project Ownership assignments-changes"
+  combiner= "OR"
+  conditions {
+      display_name = "instance count exceeded"
+      condition_threshold = {
+        aggregations {
+          alignment_period = "60s"
+          per_series_aligner = "ALIGN_RATE"
+        }
+        comparison = "COMPARISON_GT"
+        duration = "60s"
+        filter = "metric.type=\"appengine.googleapis.com/system/instance_count\" resource.type=\"gae_app\" resource.label.\"module_id\"=\"pypostgresql\""
+        threshold_value = 2
+        trigger = {
+          count = 1
+        }
+      }
+  }
+  documentation = {
+    content = "# Warning\n\n## Error message:\n\nInstance count for pypostgresql has been exceeded.\nThis alert policy is configured via Terraform."
+    mime_type = "text/markdown"
+  }
+}
+
 #Service account modifications
