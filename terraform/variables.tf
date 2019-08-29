e72bf5c89fe5b58508a56e1da2b4d47a52e7837a
RobertJLupinek <9854828608@GSA.GOV>
2019-08-29 13:34:13 -0400

    Adding stackdriver logging.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,14 @@ variable "project_id" {
   type = "string"
 }
 
+variable "notification_email" {
+  type = "string"
+}
+
+variable "cloudsql_db_name" {
+  type = "string"
+}
+
 variable "cloudsql_username" {
   type = "string"
 }
