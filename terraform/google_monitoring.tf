2d11167d092bdd16f945d566499cea9b7a86f34e
RobertJLupinek <9854828608@GSA.GOV>
2019-09-03 12:04:42 -0400

    Adding CIS 2.4,2.5,2.6,2.10, and 2.11.

--- terraform/google_monitoring.tf
+++ terraform/google_monitoring.tf
@@ -8,98 +8,69 @@ resource "google_monitoring_notification_channel" "email" {
   }
 }
 
+########################
+#Create logging metrics#
+########################
 
-resource "google_logging_metric" "create_service_account" {
-  name = "metric-create-service-account"
-  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND protoPayload.methodName:\"google.iam.admin.v1.CreateServiceAccount\""
+
+
+#CIS 2.4 Ensure log metric filter and alerts exists for Project Ownership assignments/changes
+
+resource "google_logging_metric" "cis2_4_project_owner_change" {
+  name = "cis2-4-project_owner-change"
+  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND (protoPayload.serviceName=\"cloudresourcemanager.googleapis.com\") AND (ProjectOwnership OR projectOwnerInvitee) OR (protoPayload.serviceData.policyDelta.bindingDeltas.action=\"REMOVE\" AND protoPayload.serviceData.policyDelta.bindingDeltas.role=\"roles/owner\") OR (protoPayload.serviceData.policyDelta.bindingDeltas.action=\"ADD\" AND protoPayload.serviceData.policyDelta.bindingDeltas.role=\"roles/owner\")"
   metric_descriptor {
     metric_kind = "DELTA"
     value_type = "INT64"
   }
 }
 
-resource "google_logging_metric" "iam_policy_change" {
-  name = "metric-iam-policy-change"
-  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND protoPayload.methodName:\"SetIamPolicy\""
+
+#CIS 2.5 Ensure log metric filter and alerts exists for Audit Configuration 
+
+resource "google_logging_metric" "cis2_5_audit_config_change" {
+  name = "cis2-5-audit-config-change"
+  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND protoPayload.methodName=\"SetIamPolicy\" AND protoPayload.serviceData.policyDelta.auditConfigDeltas:*"
   metric_descriptor {
     metric_kind = "DELTA"
     value_type = "INT64"
   }
 }
 
-#resource "google_logging_metric" "project_owner_change" {
-#  name = "metric-owner-change"
-#  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity" AND methodName: "google.iam.admin.v1.CreateServiceAccount\""
-#  metric_descriptor {
-#    metric_kind = "DELTA"
-#    value_type = "INT64"
-#  }
-#}
-
-# Monitoring For Max Connections
-resource "google_monitoring_alert_policy" "alert_policy_connections"{
-  display_name = "Connection limit test - App Engine - pypostgresql"
-  combiner= "OR"
-  conditions {
-      display_name = "GAE Application - Connections for ${var.project_id}, pypostgresql"
-      condition_threshold {
-        aggregations {
-          alignment_period = "60s"
-          per_series_aligner = "ALIGN_MEAN"
-        }
-        comparison = "COMPARISON_GT"
-        duration = "60s"
-        filter = "metric.type=\"appengine.googleapis.com/flex/connections/current\" resource.type=\"gae_app\" resource.label.\"project_id\"=\"${var.project_id}\" resource.label.\"module_id\"=\"pypostgresql\""
-        threshold_value = 200
-        trigger {
-          count = 1
-        }
-      }
-  }
-  documentation {
-    content = "# Warning!!!\n\nError message:\n\nThis is just a simple test to validate alerting policy configuration.\nThis alert policy is configured via Terraform."
-    mime_type = "text/markdown"
-  }
-
-  notification_channels = [
-    "${google_monitoring_notification_channel.email.name}",
-  ]
-}
 
 
-# Monitoring for number of running instances
-resource "google_monitoring_alert_policy" "alert_policy_instance_count"{
-  display_name = "Instance count max exceeded - App Engine - pypostgresql"
-  combiner= "OR"
-  conditions {
-      display_name = "instance count exceeded"
-      condition_threshold {
-        aggregations {
-          alignment_period = "60s"
-          per_series_aligner = "ALIGN_COUNT"
-        }
-        comparison = "COMPARISON_GT"
-        duration = "60s"
-        filter = "metric.type=\"appengine.googleapis.com/system/instance_count\" resource.type=\"gae_app\" resource.label.\"module_id\"=\"pypostgresql\""
-        threshold_value = 2
-        trigger {
-          count = 1
-        }
-      }
-  }
-  documentation {
-    content = "# Warning\n\n## Error message:\n\nInstance count max for pypostgresql has been exceeded.\nThis alert policy is configured via Terraform."
-    mime_type = "text/markdown"
+#CIS 2.6 Ensure log metric filter and alerts exists for Custom Role changes 
+
+resource "google_logging_metric" "cis2_6_custom_role_change" {
+  name = "cis2-6-custom-role-change"
+  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND resource.type=\"iam_role\" AND protoPayload.methodName =  \"google.iam.admin.v1.CreateRole\" OR protoPayload.methodName=\"google.iam.admin.v1.DeleteRole\" OR protoPayload.methodName=\"google.iam.admin.v1.UpdateRole\""
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
   }
 }
 
 
-# Monitoring For Changes in Project Ownership
-#resource "google_monitoring_alert_policy" "project_owner_change"{
+#CIS 2.10 - Ensure log metric filter and alerts exists for Cloud Storage IAM permission changes
+
+resource "google_logging_metric" "cis2_10_storage_iam_change" {
+  name = "cis2-10-storage-iam-change"
+  filter = "logName:\"projects/${var.project_id}/logs/cloudaudit.googleapis.com%2Factivity\" AND resource.type=gcs_bucket AND protoPayload.methodName=\"storage.setIamPermissions\""
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type = "INT64"
+  }
+}
+
+
+
+# Service account creation
+#
+#resource "google_monitoring_alert_policy" "create_service_account"{
 #  display_name = "Service account creation"
 #  combiner= "OR"
 #  conditions {
-#      display_name = "project_owner_change"
+#      display_name = "create_service_account"
 #      condition_threshold {
 #        aggregations {
 #          alignment_period = "60s"
@@ -107,7 +78,7 @@ resource "google_monitoring_alert_policy" "alert_policy_instance_count"{
 #        }
 #        comparison = "COMPARISON_GT"
 #        duration = "60s"
-#        filter = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.project_owner_change.name}\" AND resource.type=\"global\""
+#        filter = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.create_service_account.name}\" AND resource.type=\"global\""
 #        threshold_value = 0
 #        trigger {
 #          count = 1
@@ -115,63 +86,9 @@ resource "google_monitoring_alert_policy" "alert_policy_instance_count"{
 #      }
 #  }
 #  documentation {
-#    content = "# Warning\n\n## Error message:\n\nThe project ownership has changed.\nThis alert policy is configured via Terraform."
+#    content = "# Warning\n\n## Error message:\n\nNew service account has been created.\nThis alert policy is configured via Terraform."
 #    mime_type = "text/markdown"
 #  }
 #}
 
 
-# Service account creation
-
-resource "google_monitoring_alert_policy" "create_service_account"{
-  display_name = "Service account creation"
-  combiner= "OR"
-  conditions {
-      display_name = "create_service_account"
-      condition_threshold {
-        aggregations {
-          alignment_period = "60s"
-          per_series_aligner = "ALIGN_SUM"
-        }
-        comparison = "COMPARISON_GT"
-        duration = "60s"
-        filter = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.create_service_account.name}\" AND resource.type=\"global\""
-        threshold_value = 0
-        trigger {
-          count = 1
-        }
-      }
-  }
-  documentation {
-    content = "# Warning\n\n## Error message:\n\nNew service account has been created.\nThis alert policy is configured via Terraform."
-    mime_type = "text/markdown"
-  }
-}
-
-
-#IAM Policy Changes
-
-resource "google_monitoring_alert_policy" "iam_policy_change"{
-  display_name = "IAM Policy changes"
-  combiner= "OR"
-  conditions {
-      display_name = "iam_policy_change"
-      condition_threshold {
-        aggregations {
-          alignment_period = "60s"
-          per_series_aligner = "ALIGN_SUM"
-        }
-        comparison = "COMPARISON_GT"
-        duration = "60s"
-        filter = "metric.type=\"logging.googleapis.com/user/${google_logging_metric.iam_policy_change.name}\" AND resource.type=\"global\""
-        threshold_value = 0
-        trigger {
-          count = 1
-        }
-      }
-  }
-  documentation {
-    content = "# Warning\n\n## Error message:\n\nIAM Policy change.\nThis alert policy is configured via Terraform."
-    mime_type = "text/markdown"
-  }
-}
